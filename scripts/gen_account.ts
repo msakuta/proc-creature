fc682d1c8648ee2ba15b0b1f1e9207ae1f4c18b4
msakuta <masahiro.sakuta@gmail.com>
2024-05-12 19:26:19 +0900

    Allow generating an account in testnet (although aptos CLI would be easier)

--- scripts/gen_account.ts
+++ scripts/gen_account.ts
@@ -10,19 +10,19 @@ async function main() {
     const outFile = await fsPromises.open("../.aptos/config.yaml", "w");
     await outFile.write("---\nprofiles:\n");
 
+    const APTOS_NETWORK: Network = NetworkToNetworkName[process.env.APTOS_NETWORK || ""] || Network.DEVNET;
+    const config = new AptosConfig({ network: APTOS_NETWORK });
+    const aptosClient = new Aptos(config);
+
     async function printAccount(name, acc) {
         await outFile.write(`  ${name}:
     private_key: "${acc.privateKey}"
     public_key: "${acc.publicKey}"
     account: ${acc.accountAddress}
-    rest_url: "https://fullnode.devnet.aptoslabs.com"
-    faucet_url: "https://faucet.devnet.aptoslabs.com"
+    rest_url: "https://fullnode.${APTOS_NETWORK}.aptoslabs.com"
+    faucet_url: "https://faucet.${APTOS_NETWORK}.aptoslabs.com"
 `);
     }
-
-    const APTOS_NETWORK: Network = NetworkToNetworkName[process.env.APTOS_NETWORK || ""] || Network.DEVNET;
-    const config = new AptosConfig({ network: APTOS_NETWORK });
-    const aptosClient = new Aptos(config);
 
     await printAccount("default", account);
 
